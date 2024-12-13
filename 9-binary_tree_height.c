171078561fa304cbc891b12d98ada529eb616102
root <root@DESKTOP-NDVQC7F.localdomain>
2024-12-13 14:25:18 +0100

    9-binary_tree_height.c added, file.h updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,10 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-if (!tree)
+size_t left_height, right_height;
+if (tree == NULL)
 return (0);
-return (1 + _height(tree));
+left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
+right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+return ((left_height > right_height ? left_height : right_height));
 }
